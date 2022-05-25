2dfc442eeabd90c4ffee958283932a02058ae14a
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-25 16:32:35 +0200

    Ho modificato lo stile delle card

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,16 +1,46 @@
-<h1>
-    COMICS
-</h1>
-@forelse ($comics as $comic)
-    <div class="card">
-        <img src="{{$comic->thumb}}" alt="">
-        <h2>{{$comic->title}}</h2>
-        <p>{{$comic->price}}</p>
-        <p>{{$comic->series}}</p>
-        <p>{{$comic->sale_date}}</p>
-        <p>{{$comic->type}}</p>
+@extends('layouts.app')
+
+@section('content')
+    
+    <!-- titolo -->
+    <h1>
+        COMICS
+    </h1>
+    
+    <div class="central-page">
+
+        <div class="container">
+            
+            <!-- carico tutti i fumetti con i rispettivi dettagli  -->
+            @forelse ($comics as $comic)
+
+                <div class="card">
+                    <!-- foto -->
+                    <img src="{{$comic->thumb}}" alt="">
+
+                    <!-- titolo -->
+                    <h2>{{$comic->title}}</h2>
+
+                    <!-- prezzo -->
+                    <p>{{$comic->price}}</p>
+
+                    <!-- serie -->
+                    <p>{{$comic->series}}</p>
+
+                    <!-- data di uscita -->
+                    <p>{{$comic->sale_date}}</p>
+
+                    <!-- tipologia -->
+                    <p>{{$comic->type}}</p>
+                </div>
+                
+            @empty
+                <h1>No comics </h1>
+            @endforelse
+
+        </div>
+
     </div>
     
-@empty
-    <h1>No comics </h1>
-@endforelse
+@endsection
+    
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- link al css -->
+    <link rel="stylesheet" href="css/app.css">
+    <title>DC COMICS</title>
+</head>
+<body>
+
+    <main>
+        @yield('content')
+    </main>
+    
+    
+</body>
+</html>
