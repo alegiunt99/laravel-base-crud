11dee44de9890b904b4a73bdba7d2f89f2c01fa5
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-25 16:06:11 +0200

    Ho stampato tutti i comics con i dettagli

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,7 +2,15 @@
     COMICS
 </h1>
 @forelse ($comics as $comic)
-    <h2>{{$comic->title}}</h2>
+    <div class="card">
+        <img src="{{$comic->thumb}}" alt="">
+        <h2>{{$comic->title}}</h2>
+        <p>{{$comic->price}}</p>
+        <p>{{$comic->series}}</p>
+        <p>{{$comic->sale_date}}</p>
+        <p>{{$comic->type}}</p>
+    </div>
+    
 @empty
     <h1>No comics </h1>
 @endforelse
